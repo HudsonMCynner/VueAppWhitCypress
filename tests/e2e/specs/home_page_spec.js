953b9b38f8f9a94e1249db259b5ff460bfc071f3
Hudson <hudsonmachado2012@gmail.com>
2020-10-26 16:11:46 -0400

    testes basicos

--- tests/e2e/specs/home_page_spec.js
+++ tests/e2e/specs/home_page_spec.js
@@ -1,5 +1,7 @@
 describe('The Home Page', function() {
   it('successfully loads', function() {
     cy.visit('/')
+    cy.get('input.hello-world').type('Por hoje é isso pessoal!');
+    cy.get('button').click();
   });
 });
